c992f9730a8f5f77e8b487c56b301d76002d2707
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-07 19:18:12 +0800

    student introduce with class number

--- src/main/java/com/oocl/Student.java
+++ src/main/java/com/oocl/Student.java
@@ -2,7 +2,21 @@ package com.oocl;
 
 public class Student extends Person {
 
+    private String klassNumber;
+
+    public String getKlassNumber() {
+        return klassNumber;
+    }
+
+    public void setKlassNumber(String klassNumber) {
+        this.klassNumber = klassNumber;
+    }
+
     public String introduce(){
-        return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL.",this.getName(),this.getAge());
+
+        if(klassNumber.isEmpty()){
+            return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL.",this.getName(),this.getAge());
+        }
+        return String.format("My name is %s. I am %s years old. I am a student of class %s. Coding for the glory of OOCL.",this.getName(),this.getAge(),klassNumber);
     }
 }
